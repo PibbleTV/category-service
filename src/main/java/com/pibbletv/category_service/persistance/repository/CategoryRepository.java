f88cf9c0f6c50389d7b63bd02d8c4fd74d2fd507
TanzerDx <hristoganchev3@gmail.com>
2025-05-06 21:37:45 +0200

    added findByKeyword method

--- src/main/java/com/pibbletv/category_service/persistance/repository/CategoryRepository.java
+++ src/main/java/com/pibbletv/category_service/persistance/repository/CategoryRepository.java
@@ -1,11 +1,15 @@
 package com.pibbletv.category_service.persistance.repository;
 
 import com.pibbletv.category_service.persistance.entities.CategoryEntity;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface CategoryRepository extends ReactiveCrudRepository<CategoryEntity, Long> {
-
+    @Query("SELECT * FROM categories WHERE LOWER(name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Flux<CategoryEntity> findByKeyword(@Param("keyword") String keyword);
 }
