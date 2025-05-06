70ba88986f0e3977af85ca26eeee578017b2a996
TanzerDx <hristoganchev3@gmail.com>
2025-05-06 19:53:02 +0200

    added methods

--- src/main/java/com/pibbletv/category_service/persistance/repository/CategoryRepository.java
+++ src/main/java/com/pibbletv/category_service/persistance/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.pibbletv.category_service.persistance.repository;
 import com.pibbletv.category_service.persistance.entities.CategoryEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CategoryRepository extends ReactiveCrudRepository<CategoryEntity, Long> {
+
 }
