7388f6909fdc6be7ac9627296b75da502603a225
TanzerDx <hristoganchev3@gmail.com>
2025-05-06 19:53:02 +0200

    added methods

--- src/main/java/com/pibbletv/category_service/controller/CategoryController.java
+++ src/main/java/com/pibbletv/category_service/controller/CategoryController.java
@@ -3,11 +3,11 @@ package com.pibbletv.category_service.controller;
 import com.pibbletv.category_service.business.interfaces.CategoryService;
 import com.pibbletv.category_service.domain.Category;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @RestController
@@ -21,4 +21,23 @@ public class CategoryController {
     public Flux<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
+
+    @PreAuthorize("hasRole('admin')")
+    @PostMapping(value = "/addCategory")
+    public Mono<Void> addCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @PutMapping(value = "/updateCategory")
+    public Mono<Void> editCategory(@RequestBody Category category) {
+        return categoryService.updateCategory(category);
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @DeleteMapping(value = "/deleteCategory")
+    public Mono<Void> deleteCategory(@RequestParam Long categoryId) {
+        return categoryService.deleteCategory(categoryId);
+    }
 }
+
